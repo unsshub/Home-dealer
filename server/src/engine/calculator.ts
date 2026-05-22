9d3173ed8f1b6cd7c2dd19a951b75ece708d6aca
unsshub <joshmandezceo@gmail.com>
2026-05-22 17:05:15 +0100

    docs: annotate placeholder strategy registrations with TODO

--- server/src/engine/calculator.ts
+++ server/src/engine/calculator.ts
@@ -5,7 +5,7 @@ export type DSCRCalculator = (input: DSCRInput) => DSCRResult;
 
 export const registry: Record<Strategy, DSCRCalculator> = {
   buy_and_hold: buyAndHoldCalculate,
-  brrrr: buyAndHoldCalculate,
-  fix_and_flip: buyAndHoldCalculate,
-  str: buyAndHoldCalculate,
+  brrrr: buyAndHoldCalculate,       // TODO: replace with brrrrCalculate in Task 3
+  fix_and_flip: buyAndHoldCalculate, // TODO: replace with fixAndFlipCalculate in Task 4
+  str: buyAndHoldCalculate,          // TODO: replace with strCalculate in Task 5
 };
